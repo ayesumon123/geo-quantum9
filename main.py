b3727e4bda955cce7b6df0d2f78c229efafc4933
Eithandarphyo51 <eithandarprogram@gmail.com>
2018-07-15 22:11:48 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, url_for
+
+myapp = Flask(__name__)
+@myapp.route("/")
+def hello():
+    return render_template("index.html")
+
+@myapp.route("/compose")
+def compose():
+    return render_template("compose.html")
+if __name__ == "__main__":
+    myapp.run(debug=True)
+
